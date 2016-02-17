4c8aebffc7fee0179eefca25edde2a994f69b752
Marcos Barbero <marcos.hgb@gmail.com>
2016-02-17 19:34:06 -0200

    Organize imports

--- catalog-service/src/main/java/com/demo/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/demo/CatalogServiceApplication.java
@@ -2,8 +2,6 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
