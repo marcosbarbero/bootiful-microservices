d1dc4047c5375f7f24806672648b9c4037ac3b97
Marcos Barbero <marcos.hgb@gmail.com>
2016-07-13 13:26:50 -0300

    Upgrade Brixton.SR3

--- gateway-service/src/main/java/com/demo/GatewayApplication.java
+++ gateway-service/src/main/java/com/demo/GatewayApplication.java
@@ -3,12 +3,13 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author Marcos Barbero
  */
 @EnableZuulProxy
-//@EnableResourceServer
+@EnableResourceServer
 @SpringCloudApplication
 public class GatewayApplication {
 
